2ab607ada5fb808d0e3a76166470b7bc956ead00
Arijeet Choudhury <arijeetchoudhury100@gmail.com>
2018-07-02 12:31:29 -0400

    correct commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear();
+})
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next)=>{
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFileSync('server.log',log+'\n');
+	next();
+})
+app.get('/',(req,res) => {
+	res.render('home.hbs',{
+		pageTitle : 'Home Page',
+	});
+});
+app.get('/about',(req,res) => {
+	res.render('about.hbs',{
+		pageTitle : 'About Page',
+	});
+})
+app.listen(3000,() => {
+ 	console.log("server is up and running!");
+ });
